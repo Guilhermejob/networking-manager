0cec54f624d25d8ea2fca17ce142adacccc456ae
Guilherme Job <guilhermejob047@gmail.com>
2025-11-10 07:03:47 -0300

    tests: incluido configs basicas de testes

---
+++ backend/jest.config.js
@@ -0,0 +1,22 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.ts'],
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/controllers/**/*.ts',
+    'src/services/**/*.ts',
+    'src/routes/**/*.ts',
+    '!src/lib/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
